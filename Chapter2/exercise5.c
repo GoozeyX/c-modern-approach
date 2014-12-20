790204bbc8bcb408c8c91d15de56dc5060d83dd0
Alexander Brovman <alexander.brovman@gmail.com>
2014-12-20 15:48:49 +0100

    update2

---
+++ Chapter2/exercise5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float x;
+    printf("Please input a number: ");
+    scanf("%f", &x);
+
+    float val = (3 * (x * x * x * x * x)) +
+                (2 * (x * x * x * x)) -
+                (5 * (x * x * x)) -
+                (x * x) +
+                (7 * x) -
+                6;
+
+    printf("Value of polynomial is: %f\n", val);
+    return 0;
+}
